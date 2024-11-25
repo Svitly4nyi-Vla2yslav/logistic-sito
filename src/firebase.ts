7a22c792a11685ee1ce82bd757fccd03931c5b78
Svitly4nyi-Vla2yslav <126347711+Svitly4nyi-Vla2yslav@users.noreply.github.com>
2024-11-25 10:50:26 +0100

    fix error

--- src/firebase.ts
+++ src/firebase.ts
@@ -10,16 +10,16 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBI5iEHOMKfM5gR9PJCLDAZeVBGo1qbdtg",
-  authDomain: "logistic-79c67.firebaseapp.com",
-  databaseURL: "https://logistic-79c67-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "logistic-79c67",
-  storageBucket: "logistic-79c67.firebasestorage.app",
-  messagingSenderId: "829163065021",
-  appId: "1:829163065021:web:83a4f9713b6a98939cf1d4",
-  measurementId: "G-FYDN94CL0V"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const analytics = getAnalytics(app);
