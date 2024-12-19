ce95f74d46f63e49cf4d9ebba838a5d5be8f830c
Svitly4nyi-Vla2yslav <126347711+Svitly4nyi-Vla2yslav@users.noreply.github.com>
2024-12-19 01:37:05 +0100

    add styled

--- src/components/AboutUsComponents/AboutInfo.tsx
+++ src/components/AboutUsComponents/AboutInfo.tsx
@@ -31,46 +31,38 @@ const AboutInfo: React.FC = () => {
   }, []);
   return (
     <AboutContainer>
-      <TitelSpan data-aos="fade-up">{t('our_mission')}</TitelSpan>
-      <AboutTitel data-aos="fade-down">
+      <TitelSpan>{t('our_mission')}</TitelSpan>
+      <AboutTitel>
         <Trans
           i18nKey="about_title"
           components={{ highlight: <AboutSpan /> }}
         />
       </AboutTitel>
-      <TeamContainer data-aos="flip-left">
+      <TeamContainer>
         <TeamText>{t('team_text')}</TeamText>
         <VideoTeamWrapper>
-          <VideoTeamWork autoPlay loop muted src={Team} />
+          <VideoTeamWork autoPlay loop muted src={Team} data-aos="zoom-in" />
         </VideoTeamWrapper>
       </TeamContainer>
-      <InfoWrapper data-aos="flip-up">
-        <ImageWrapper>
-          <ImageAbout src={Image} alt="Europa" data-aos="zoom-in" />{' '}
+      <InfoWrapper>
+        <ImageWrapper data-aos="zoom-in">
+          <ImageAbout src={Image} alt="Europa" />{' '}
         </ImageWrapper>
         <ImageWrapper>
-          <TextColorise
-            data-aos="fade-up"
-            data-aos-anchor-placement="center-bottom"
-          >
+          <TextColorise data-aos="fade-up" data-aos-duration="3000">
             Providing Full Range Of Transportation Worldwide
           </TextColorise>
-          <TitleAbout data-aos="fade-up" data-aos-anchor-placement="top-center">
+          <TitleAbout data-aos="fade-up" data-aos-duration="4000">
             Reliable Logistic & Transport Solutions Saves Your Time and money!
           </TitleAbout>
-          <TextWrapper
-            data-aos="fade-left"
-            data-aos-anchor="#example-anchor"
-            data-aos-offset="1500"
-            data-aos-duration="1500"
-          >
+          <TextWrapper>
             <TextWrapper>
-              <TextInfo>
+              <TextInfo data-aos="fade-up" data-aos-duration="3000">
                 Seto logistic Group is a representative logistics operator
                 providing full range of service in the sphere of customs
                 clearance transportation worldwide for any cargo
               </TextInfo>
-              <TextInfo>
+              <TextInfo data-aos="fade-up" data-aos-duration="3000">
                 We pride ourselves on providing the best transport and shipping
                 services available allover the world. Our skilled personnel,
                 utilising the latest communications, tracking and combined with
@@ -79,15 +71,19 @@ const AboutInfo: React.FC = () => {
             </TextWrapper>
             <ul>
               <li>
-                <TextList>✔️ Quality </TextList>
-                <TextInfo>
+                <TextList data-aos="fade-up" data-aos-duration="3000">
+                  ✔️ Quality{' '}
+                </TextList>
+                <TextInfo data-aos="fade-up" data-aos-duration="3000">
                   Following the quality of our service thus having gained trust
                   of our many clients.
                 </TextInfo>
               </li>
               <li>
-                <TextList>✔️ Rellability</TextList>
-                <TextInfo>
+                <TextList data-aos="fade-up" data-aos-duration="3000">
+                  ✔️ Rellability
+                </TextList>
+                <TextInfo data-aos="fade-up" data-aos-duration="3000">
                   We provide with cargo safety throughout all the stages of our
                   delivery process..
                 </TextInfo>
